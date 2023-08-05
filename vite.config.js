4657ae0400327b5946f04441ff15f682a2b32b05
Haily3344 <holyhaily288@gmail.com>
2023-08-05 23:41:22 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //開發中(npm run dev) 產品路徑(npm run build)
+  //base: process.env.NODE_EVN === 'production' ? '/react-gh-sample/' : '/',
+  base: '/react-gh-sample/',
   plugins: [react()],
 })
